1016906d64cfbd62f16e58ea2f3c26c399ccf96d
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2025-11-15 13:47:24 -0300

    Iniciando projeto NutriAI

---
+++ src/main/java/br/com/ehmf/NutriAI/NutriAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ehmf.NutriAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriAiApplication.class, args);
+	}
+
+}
